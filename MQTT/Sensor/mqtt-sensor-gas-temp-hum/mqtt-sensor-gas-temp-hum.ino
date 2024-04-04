b8dd10c2fef0231b47dd5d2f6beb4b3aace3874f
TetoSolis <164871660+TetoSolis@users.noreply.github.com>
2024-04-04 07:48:51 +0200

    Update mqtt-sensor-gas-temp-hum.ino

--- MQTT/Sensor/mqtt-sensor-gas-temp-hum/mqtt-sensor-gas-temp-hum.ino
+++ MQTT/Sensor/mqtt-sensor-gas-temp-hum/mqtt-sensor-gas-temp-hum.ino
@@ -1,81 +1,152 @@
-#include <DHT11.h>
-#include <MQ135.h>
+static const char ca_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDXzCCAkegAwIBAgIUUqVmTigzEdD+eM3cbSmzJSAjDVYwDQYJKoZIhvcNAQEL
+BQAwPzELMAkGA1UEBhMCRlIxDDAKBgNVBAgMA0JGQzEUMBIGA1UEBwwLTW9udGJl
+bGlhcmQxDDAKBgNVBAoMA0lVVDAeFw0yNDA0MDIwODUwNTBaFw0yOTA0MDIwODUw
+NTBaMD8xCzAJBgNVBAYTAkZSMQwwCgYDVQQIDANCRkMxFDASBgNVBAcMC01vbnRi
+ZWxpYXJkMQwwCgYDVQQKDANJVVQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+AoIBAQCyB9umJs1hPCWJ3ARI2XN8R+dBYuLK8HPV+NRTt9AtOMh98GOddvc7OQFK
+lHYERBRksKotzLvV9PPMKsqP+1cfyOUSAj/yPVgIJoBkCIsT5yfWtYlX2ZpIp11G
+MwaGvG2j8ws28IZXwfMyGwYQ1eMT9pFY3ukcKAC/nLs9/sk5ja9LXLUaaKQVJD9r
+elBaqLAEFFE8bmJi9gQNqXGnO5w2e8veemP7d1Sgv3qyjNM+aJRhq8Kq+1GlM4Hs
+Hgt/4tCJdaaWfkv9IoJy9HbKOlg3kNB2MGab2CZCR0dMRuWeFOtgKW6dAvRx8E/h
+M40oNqe1ZbZARwOeQHZckKPzF9sxAgMBAAGjUzBRMB0GA1UdDgQWBBR5BM1A0mzb
+M4ZYXX76by6HHoN9CjAfBgNVHSMEGDAWgBR5BM1A0mzbM4ZYXX76by6HHoN9CjAP
+BgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAUMXELqYKkawUiPwrU
+DafNbBnggWUGAuU4oiQe8dAGk6/usLq5hx64E4SU3jECRLiK2ifzE2wlEMBeVv2e
+QGZIeL6k7JTDFuS+fTvokD3SQVn5qy0VhalqQF3dmtvg7m+c6I50ZMVEBsXdniMB
+tA3XTyJeSaGWGtN2SMhIYrhJjpx/NcsLDqfh07B9txKJaKtfMiMlGI5Z/3PQ/7gr
+ohxPIjHvomi03C/YA7PP1CuZdzerT9z6WReXtRcG40ddsspKg6MqnxeybtP+qoT2
+POZe/GCOAGGsJS6MpLYmsM3XzG3kEJggJrpAQWUvl59b9m9G1ePJ+FfE/oHyb4PE
+H6gS
+-----END CERTIFICATE-----
+)EOF";
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <MQ135.h>
+#include <DHT11.h>
 
-// Déclaration des broches
 #define DHTPIN 2 // Broche du capteur DHT11 (GPIO2 ou D4 pour ESP8266)
-#define ANALOG_PIN A0 // Broche analogique à laquelle le capteur MQ135 est connecté
-
-// Initialisation des objets
+const int ANALOG_PIN = A0; // Broche analogique à laquelle le capteur MQ135 est connecté
+MQ135 gasSensor = MQ135(ANALOG_PIN);
 DHT11 dht11(DHTPIN);
-MQ135 gasSensor(ANALOG_PIN);
-WiFiClient espClient;
-PubSubClient client(espClient);
 
-// Configuration du réseau WiFi
+// Update these with values suitable for your network.
+
 const char* ssid = "GRP-1-IOM";
 const char* password = "Porygon-Z#1";
-
-// Configuration MQTT
 const char* mqtt_server = "192.168.1.50";
+const char* MqttUser = "GRP-1-IOM";
+const char* MqttPass = "Porygon-Z#1";
+
 const char* mqtt_topic_temp = "/Sensor/Temperature/";
 const char* mqtt_topic_humid = "/Sensor/Humidity/";
 const char* mqtt_topic_gas = "/Sensor/Gas/";
 
-void setup() {
-    // Initialisation de la communication série pour le débogage et l'affichage des données
-    Serial.begin(115200);
+BearSSL::WiFiClientSecure espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
 
-    // Configuration du client WiFi
-    setup_wifi();
+void setClock()
+{
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
 
-    // Configuration du serveur MQTT
-    client.setServer(mqtt_server, 1883);
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
 }
 
 void setup_wifi() {
-    delay(10);
-    Serial.println();
-    Serial.print("Connexion au réseau ");
-    Serial.println(ssid);
 
-    WiFi.begin(ssid, password);
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-    Serial.println("");
-    Serial.println("WiFi connecté");
-    Serial.println("Adresse IP: ");
-    Serial.println(WiFi.localIP());
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
 }
 
 void reconnect() {
-    // Boucle de reconnexion au serveur MQTT
-    while (!client.connected()) {
-        Serial.print("Tentative de connexion au serveur MQTT...");
-        if (client.connect("ESP-Gas-Hum-Temp", "j", "j")) {
-            Serial.println("connecté");
-        } else {
-            Serial.print("échec, rc=");
-            Serial.print(client.state());
-            Serial.println(" réessayez dans 5 secondes");
-            delay(5000);
-        }
+  char err_buf[256];
+  
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), MqttUser, MqttPass)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.println(client.state());
+      espClient.getLastSSLError(err_buf, sizeof(err_buf));
+      Serial.print("SSL error: ");
+      Serial.println(err_buf);
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  BearSSL::X509List *serverTrustedCA = new BearSSL::X509List(ca_cert);
+  espClient.setTrustAnchors(serverTrustedCA);
+  espClient.setInsecure();
+  setup_wifi();
+  setClock(); // Required for X.509 validation
+  client.setServer(mqtt_server, 8883);
+  client.setCallback(callback);
 }
 
 void loop() {
-    // Vérification de la connexion MQTT
-    if (!client.connected()) {
-        reconnect();
-    }
-    client.loop();
 
-    // Lecture de la température et de l'humidité depuis le capteur DHT11
+  if (!client.connected()) {
+    reconnect();
+  }
+  delay(100);
+  // Lecture de la température et de l'humidité depuis le capteur DHT11
     int temperature = 0;
     int humidity = 0;
     int result = dht11.readTemperatureHumidity(temperature, humidity);
@@ -90,13 +161,9 @@ void loop() {
         Serial.println(" %");
 
         // Publication des valeurs de température et d'humidité sur MQTT
-        if (!client.connected()) {
-        reconnect();
-        }
+        client.loop();
         client.publish(mqtt_topic_temp, String(temperature).c_str());
-        if (!client.connected()) {
-        reconnect();
-        }
+        client.loop();
         client.publish(mqtt_topic_humid, String(humidity).c_str());
     } else {
         // Affichage du message d'erreur
@@ -104,16 +171,16 @@ void loop() {
     }
 
     // Lecture de la valeur corrigée de CO2 depuis le capteur MQ135
-    float ppm = gasSensor.getCorrectedPPM(temperature, humidity)/100;
+    float ppm = gasSensor.getCorrectedPPM(temperature, humidity) / 100;
 
     // Affichage des valeurs sur le moniteur série
     Serial.print("Valeur CO2 corrigée: ");
     Serial.println(ppm);
 
-    if (!client.connected()) {
-        reconnect();
-    }
     // Publication de la valeur de CO2 sur MQTT
+    client.loop();
     client.publish(mqtt_topic_gas, String(ppm).c_str());
+
+    // Pause d'une minute avant la prochaine lecture
     delay(60000);
 }
